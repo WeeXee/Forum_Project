6af6132db4dc0571a9602912a8b0e6b8f745c944
pauli <pauline.cabee@ynov.com>
2022-06-07 14:33:29 +0200

    redirect pages

--- functions/post.go
+++ functions/post.go
@@ -3,7 +3,6 @@ package functions
 import (
 	"fmt"
 	"net/http"
-	"strings"
 	"text/template"
 )
 
@@ -12,8 +11,6 @@ type StructPost struct {
 	IDuser      int
 	post        string
 	title       string
-	like        int
-	dislike     int
 }
 
 func Post(w http.ResponseWriter, r *http.Request) {
@@ -31,37 +28,4 @@ func Post(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Print("error")
 	}
-}
-
-func WordLimiter(s string, limit int) string {
-
-	if strings.TrimSpace(s) == "" {
-		return s
-	}
-
-	// convert string to slice
-	strSlice := strings.Fields(s)
-
-	// count the number of words
-	numWords := len(strSlice)
-
-	var result string
-
-	if numWords > limit {
-		// convert slice/array back to string
-		result = strings.Join(strSlice[0:limit], " ")
-
-		// the three dots for end characters are optional
-		// you can change it to something else or remove this line
-		result = result + "..."
-	} else {
-
-		// the number of limit is higher than the number of words
-		// return default or else will cause
-		// panic: runtime error: slice bounds out of range
-		result = s
-	}
-
-	return string(result)
-
 }
--- main.go
+++ main.go
@@ -12,10 +12,41 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("template/index.html")
 	t.Execute(w, r)
 }
+func Comedy(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("template/comedy.html")
+	t.Execute(w, r)
+}
+
+func Docu(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("template/docu.html")
+	t.Execute(w, r)
+}
+
+func Drama(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("template/drama.html")
+	t.Execute(w, r)
+}
+
+func Horror(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("template/horror.html")
+	t.Execute(w, r)
+}
+
+func Romantic(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("template/romantic.html")
+	t.Execute(w, r)
+}
+
+func SF(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("template/SF.html")
+	t.Execute(w, r)
+}
 
 func main() {
 	http.HandleFunc("/", functions.Post)
+	http.HandleFunc("/1", functions.Login)
 	fmt.Printf("Starting server got testing \n")
+	fmt.Println("Go to this adress: localhost:8080")
 	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
 	http.Handle("/static/css/", http.StripPrefix("/static/css/", http.FileServer(http.Dir("./static/css"))))
 	if err := http.ListenAndServe(":8080", nil); err != nil {
