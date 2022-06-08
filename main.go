2f099fd108f27c26e8de01308fd2ae5267bb1bd9
pauli <pauline.cabee@ynov.com>
2022-06-08 15:53:38 +0200

    dev v1 html

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	_ "github.com/go-sql-driver/mysql"
-	"unicode"
-
-	_ "github.com/mattn/go-sqlite3"
-
-	"Forum/database_sqlite"
 	"Forum/functions"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("template/index.html")
-	t.Execute(w, r)
+	functions.Post(w, r)
 }
 func Comedy(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("template/comedy.html")
@@ -48,80 +42,21 @@ func SF(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	database_sqlite.DatabaseLogin()
-	database_sqlite.DatabasePost()
-	database_sqlite.DatabaseComment()
-	arrayComment := database_sqlite.GetComment()
-	fmt.Println(arrayComment)
-	http.HandleFunc("/", functions.Post)
+
+	http.HandleFunc("/", Index)
 	http.HandleFunc("/1", functions.Login)
+	http.HandleFunc("/comedy", Comedy)
+	http.HandleFunc("/docu", Docu)
+	http.HandleFunc("/drama", Drama)
+	http.HandleFunc("/horror", Horror)
+	http.HandleFunc("/romantic", Romantic)
+	http.HandleFunc("/SF", SF)
 	fmt.Printf("Starting server got testing \n")
 	fmt.Println("Go to this adress: localhost:8080")
 	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.Handle("/static/css/", http.StripPrefix("/static/css/", http.FileServer(http.Dir("./static/css"))))
-
-	tpl, _ = template.ParseGlob("template/*.html")
-	var err error
-	if err != nil {
-		panic(err.Error())
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
 	}
-	http.HandleFunc("/register", registerHandler)
-	http.HandleFunc("/registerauth", registerAuthHandler)
-	http.ListenAndServe("localhost:8080", nil)
-}
-
-//*vanessa partie*//
-
-var tpl *template.Template
-
-// registerHandler serves form for registring new users
-func registerHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("*****registerHandler running*****")
-	tpl.ExecuteTemplate(w, "register.html", nil)
-}
-
-// creates new user
-func registerAuthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("*****registerAuthHandler running*****")
-	r.ParseForm()
-	//Create Username
-	username := r.FormValue("username")
-	var nameAlphaNumeric = true
-	for _, char := range username {
-		if unicode.IsLetter(char) == false && unicode.IsNumber(char) == false {
-			nameAlphaNumeric = false
-		}
-	}
-	var nameLength bool
-	if 4 <= len(username) && len(username) <= 20 {
-		nameLength = true
-	}
-	// Create password
-	password := r.FormValue("password")
-	fmt.Println("password:", password, "\npswdLength:", len(password))
-	var pswdLowercase, pswdUppercase, pswdNumber, pswdSpecial, pswdLength, pswdNoSpaces bool
-	pswdNoSpaces = true
-	for _, char := range password {
-		switch {
-		case unicode.IsLower(char):
-			pswdLowercase = true
-		case unicode.IsUpper(char):
-			pswdUppercase = true
-		case unicode.IsNumber(char):
-			pswdNumber = true
-		case unicode.IsPunct(char) || unicode.IsSymbol(char):
-			pswdSpecial = true
-		case unicode.IsSpace(int32(char)):
-			pswdNoSpaces = false
-		}
-	}
-	if 5 < len(password) && len(password) < 30 {
-		pswdLength = true
-	}
-	fmt.Println("pswdLowercase:", pswdLowercase, "\npswdUppercase:", pswdUppercase, "\npswdNumber:", pswdNumber, "\npswdSpecial:", pswdSpecial, "\npswdLength:", pswdLength, "\npswdNoSpaces:", pswdNoSpaces, "\nnameAlphaNumeric:", nameAlphaNumeric, "\nnameLength:", nameLength)
-	if !pswdLowercase || !pswdUppercase || !pswdNumber || !pswdSpecial || !pswdLength || !pswdNoSpaces || !nameAlphaNumeric || !nameLength {
-		tpl.ExecuteTemplate(w, "register.html", "please check username and password criteria")
-		return
-	}
-	fmt.Fprint(w, "congrats, your account has been successfully created")
 }
